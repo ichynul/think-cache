0cb9f077b35fe092d715acefc7637eb5bc470606
ichynul <ichynul@163.com>
2025-08-07 13:15:49 +0800

    支持 predis

--- src/driver/Redis.php
+++ src/driver/Redis.php
@@ -61,12 +61,28 @@ class Redis extends Driver
             $this->options = array_merge($this->options, $options);
         }
 
-        $this->handler = new \Redis;
-        $this->handler->connect($this->options['host'], (int) $this->options['port'], (int) $this->options['timeout']);
-        if ('' != $this->options['password']) {
-            $this->handler->auth($this->options['password']);
+        if (extension_loaded('redis')) {
+            $this->handler = new \Redis;
+            $this->handler->connect($this->options['host'], (int) $this->options['port'], (int) $this->options['timeout']);
+            if ('' != $this->options['password']) {
+                $this->handler->auth($this->options['password']);
+            }
+        } elseif (class_exists('\Predis\Client')) {
+            $params = [];
+            foreach ($this->options as $key => $val) {
+                if (in_array($key, ['aggregate', 'cluster', 'connections', 'exceptions', 'prefix', 'profile', 'replication', 'parameters'])) {
+                    $params[$key] = $val;
+                    unset($this->options[$key]);
+                }
+            }
+            if ('' == $this->options['password']) {
+                unset($this->options['password']);
+            }
+            $this->handler = new \Predis\Client($this->options, $params);
+            $this->options['prefix'] = '';
+        } else {
+            throw new BadFunctionCallException('not support: redis');
         }
-
         if (0 != $this->options['select']) {
             $this->handler->select((int) $this->options['select']);
         }
@@ -234,7 +250,11 @@ class Redis extends Driver
      */
     public function close()
     {
-        $this->handler->close();
+        if (method_exists($this->handler, 'close')) {
+            $this->handler->close();
+        } else {
+            $this->handler->quit();
+        }
         $this->handler = null;
     }
 }
